9df2c3ed376d392835a69f1af34b516d0c1ac1af
Annie Liao <5109918+liaowow@users.noreply.github.com>
2025-08-20 11:32:29 -0400

    Fix asset paths for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://annieliao.com',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/'
 });
